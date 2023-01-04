91a20a1e1b96bfad48d949dbb65c2bf4a56e3d5e
Phan Trí Nguyên <74893651+phantringuyen@users.noreply.github.com>
2023-01-04 16:58:47 +0700

    Update AddCategory.js
    thực hiện việc thêm một số categories vào, trình bày theo OOP để code dễ đọc cho người lập trình

--- frontend/src/admin/AddCategory.js
+++ frontend/src/admin/AddCategory.js
@@ -4,12 +4,15 @@ import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 import { createCategory } from "./apiAdmin";
 
+// khi một người admin muốn thêm categories vào trang web các khóa học
+// đây là phương thức để thêm categories
 const AddCategory = () => {
     const [name, setName] = useState("");
     const [error, setError] = useState(false);
     const [success, setSuccess] = useState(false);
 
     // destructure user and token from localstorage
+    //để xác nhận categories hợp lệ
     const { user, token } = isAuthenticated();
 
     const handleChange = e => {
@@ -17,6 +20,7 @@ const AddCategory = () => {
         setName(e.target.value);
     };
 
+    //nhấn submit để hoàn thành việc thêm categories
     const clickSubmit = e => {
         e.preventDefault();
         setError("");
@@ -32,6 +36,7 @@ const AddCategory = () => {
         });
     };
 
+    //khi người dùng tạo một categories bao gồm: tên categories và nhấn để submit việc thêm
     const newCategoryFom = () => (
         <form onSubmit={clickSubmit}>
             <div className="form-group">
@@ -49,18 +54,21 @@ const AddCategory = () => {
         </form>
     );
 
+    // khi một categories được thêm hoàn thành thì thể hiện kết quả thêm thành công cho người admin
     const showSuccess = () => {
         if (success) {
             return <h3 className="text-success">{name} is created</h3>;
         }
     };
 
+    // ngược lại thì thể hiện kết quả việc thêm thất bại vì categories tránh bị trùng lặp
     const showError = () => {
         if (error) {
             return <h3 className="text-danger">Category should be unique</h3>;
         }
     };
 
+    //cancel (quay lại màn hình chính) việc thêm categories của admin
     const goBack = () => (
         <div className="mt-5">
             <Link to="/admin/dashboard" className="text-warning">
