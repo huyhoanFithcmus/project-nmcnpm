a498ddfc4ab19bf25f0877ea96db5c94ca3898e5
vtLam123 <92106993+vtLam123@users.noreply.github.com>
2023-01-04 18:13:22 +0700

    update order
    update order.

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -18,23 +18,6 @@ exports.orderById = (req, res, next, id) => {
         });
 };
 
-
-// exports.create = (req, res) => {
-//     // console.log("CREATE ORDER: ", req.body);
-//     req.body.order.user = req.profile;
-//     const order = new Order(req.body.order);
-//     order.save((error, data) => {
-//         if (error) {
-//             return res.status(400).json({
-//                 error: errorHandler(error)
-//             });
-//         }
-//         res.json(data);
-//     });
-// };
-
-
-
 exports.create = (req, res) => {
     console.log('CREATE ORDER: ', req.body);
     req.body.order.user = req.profile;
