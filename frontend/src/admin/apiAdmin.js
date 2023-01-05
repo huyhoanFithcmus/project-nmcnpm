f3d30a9c170c9da30c76c79bd1e678a7551f3eff
Phan Trí Nguyên <74893651+phantringuyen@users.noreply.github.com>
2023-01-05 09:27:43 +0700

    Update apiAdmin.js

--- frontend/src/admin/apiAdmin.js
+++ frontend/src/admin/apiAdmin.js
@@ -1,5 +1,7 @@
 import { API } from '../config';
 
+
+/* Các thao tác vs một Category */
 // tạo mục Filter by categories cho người dùng lọc
 export const createCategory = (userId, token, category) => {
     return fetch(`${API}/category/create/${userId}`, {
@@ -17,6 +19,30 @@ export const createCategory = (userId, token, category) => {
         .catch(err => {
             console.log(err);
         });
+};
+
+// khi thực hiện việc tạo một category với một định danh ID duy nhất tương ứng
+export const getCategory = categoryId => {
+    return fetch(`${API}/category/${categoryId}`, {
+        method: 'GET'
+    })
+    // thành công thì trả về giá trị định danh
+        .then(response => {
+            return response.json();
+        })
+        // ngược lại khi ID ko hợp lệ thì báo lỗi
+        .catch(err => console.log(err));
+};
+
+//tương ứng vs nhiều categories hay một danh sách các category hợp lệ
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
 
 // cập nhật mục Filter by categories khi người dùng lọc khóa học
@@ -37,6 +63,7 @@ export const updateCategory = (categoryId, userId, token, category) => {
         .catch(err => console.log(err));
 };
 
+/* Các thao tác vs một sản phẩm */
 //tạo khóa học và xác thực một khóa học (ko bị trùng lặp)
 export const createProduct = (userId, token, product) => {
     return fetch(`${API}/product/create/${userId}`, {
@@ -47,35 +74,19 @@ export const createProduct = (userId, token, product) => {
         },
         body: product
     })
+    // thành công thì trả về giá trị định danh
         .then(response => {
             return response.json();
         })
+        // ngược lại khi ID của sản phẩm ko hợp lệ thì báo lỗi
         .catch(err => {
             console.log(err);
         });
 };
 
-//
-export const getCategory = categoryId => {
-    return fetch(`${API}/category/${categoryId}`, {
-        method: 'GET'
-    })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
-};
 
-//tương ứng vs nhiều categories
-export const getCategories = () => {
-    return fetch(`${API}/categories`, {
-        method: 'GET'
-    })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
-};
+
+
 
 // show ra danh sách các sản phẩm khóa học trên trang web
 export const listOrders = (userId, token) => {
@@ -141,6 +152,8 @@ export const getProducts = () => {
         .catch(err => console.log(err));
 };
 
+// xóa một sản phẩm (khóa học) ra khỏi danh sách bán hàng trên trang web
+// cần xác thực đc ID của khóa học, ID của người dùng (là admin hay user)
 export const deleteProduct = (productId, userId, token) => {
     return fetch(`${API}/product/${productId}/${userId}`, {
         method: 'DELETE',
@@ -166,6 +179,9 @@ export const getProduct = productId => {
         .catch(err => console.log(err));
 };
 
+//cập nhật thông tin của khóa học
+// các thông tin cần xác thực: ID của khóa học, ID của người dùng (là admin hay user)
+// token và sản phẩm có tồn tại trong danh sách khóa học ko
 export const updateProduct = (productId, userId, token, product) => {
     return fetch(`${API}/product/${productId}/${userId}`, {
         method: 'PUT',
