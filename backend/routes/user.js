89920b254eb67cedd362fb694e90ffa15cbbed55
Bùi Lâm <120543813+btlam02@users.noreply.github.com>
2023-01-05 10:00:08 +0700

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+//cal controllers
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-
 const { userById, read, update, purchaseHistory } = require('../controllers/user');
 
 router.get('/secret', requireSignin, (req, res) => {
@@ -10,7 +9,7 @@ router.get('/secret', requireSignin, (req, res) => {
         user: 'got here yay'
     });
 });
-
+//request
 router.get('/user/:userId', requireSignin, isAuth, read);
 router.put('/user/:userId', requireSignin, isAuth, update);
 router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
