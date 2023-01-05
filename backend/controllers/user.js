14c3bc8df5359e6a3613d104c25ecd10bfbf10d1
Bùi Lâm <120543813+btlam02@users.noreply.github.com>
2023-01-05 10:04:26 +0700

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,20 +20,7 @@ exports.read = (req, res) => {
     return res.json(req.profile);
 };
 
-// exports.update = (req, res) => {
-//     console.log('user update', req.body);
-//     req.body.role = 0; // role will always be 0
-//     User.findOneAndUpdate({ _id: req.profile._id }, { $set: req.body }, { new: true }, (err, user) => {
-//         if (err) {
-//             return res.status(400).json({
-//                 error: 'You are not authorized to perform this action'
-//             });
-//         }
-//         user.hashed_password = undefined;
-//         user.salt = undefined;
-//         res.json(user);
-//     });
-// };
+
 
 exports.update = (req, res) => {
     // console.log('UPDATE USER - req.user', req.user, 'UPDATE DATA', req.body);
