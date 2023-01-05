62ae554862757d5b70b24a7554b5550b068444e6
Mannotcold <man2082002@gmail.com>
2023-01-06 00:54:38 +0700

    Update user

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 const uuidv1 = require('uuid/v1');
 
+//model user
 const userSchema = new mongoose.Schema(
     {
         name: {
