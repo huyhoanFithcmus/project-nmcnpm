13d6a14d52e78503bf84ecc88d96471fefb600bc
vtLam123 <92106993+vtLam123@users.noreply.github.com>
2023-01-05 12:06:37 +0700

    update product
    short code

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -199,9 +199,6 @@ exports.listBySearch = (req, res) => {
     let skip = parseInt(req.body.skip);
     let findArgs = {};
 
-    // console.log(order, sortBy, limit, skip, req.body.filters);
-    // console.log("findArgs", findArgs);
-
     for (let key in req.body.filters) {
         if (req.body.filters[key].length > 0) {
             if (key === 'price') {
