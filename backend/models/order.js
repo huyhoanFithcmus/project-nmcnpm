7b400ac76006cee57c5f8fd0ee8d2048fa974b83
Mannotcold <man2082002@gmail.com>
2023-01-06 17:30:00 +0700

    other

--- backend/models/order.js
+++ backend/models/order.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const { ObjectId } = mongoose.Schema;
 
+
 const CartItemSchema = new mongoose.Schema(
   {
     product: { type: ObjectId, ref: "Product" },
